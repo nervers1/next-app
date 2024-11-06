4c99bf7ebfa6eb7befb5cf272d80ac2b06ae46cc
Formulate <nervers1@gmail.com>
2024-11-06 13:01:43 +0900

    Using Mutex ...

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,27 @@
-mod module1;
-mod module2;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
-
-
-mod inner_mod {
-    
-    pub fn call_super() {
-        super::extern_fn();
-    }
-
-    pub fn inner() {
-        println!("Inner Module!!!");
-    }
-}
 
 fn main() {
-    
-    module1::mod_file();
-    module2::mode_dir();
-    inner_mod::inner();
-    inner_mod::call_super();
+  let counter = Arc::new(Mutex::new(0));
+  let mut handles = vec![];
 
-    let x = 5;
-    let y = x;
+  for _ in 0..10 {
+    let counter = Arc::clone(&counter);
+    let handle = thread::spawn(move || {
+        let mut num = counter.lock().unwrap();
 
-    println!("값이 뭐냐면요... x: {x}, y: {y}");
-}
+        *num += 1;
+    });
 
-fn extern_fn() {
-    println!("call by super and outside of module");
+    handles.push(handle);
+
+  }
+
+  for handle in handles {
+    handle.join().unwrap();
+  }
+
+  println!("Result: {}", *counter.lock().unwrap());
+
 }
--- src/module1.rs
+++ src/module1.rs
@@ -2,3 +2,10 @@ pub fn mod_file() {
     println!("mod_file !");
 }
 
+#[derive(Debug)]
+pub struct User {
+    pub username: String,
+    pub email: String,
+    pub sing_in_count: u64,
+    pub active: bool
+}
