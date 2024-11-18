d901bfcf5936723ff6b2e997b958eda6b495ef83
Formulate <nervers1@gmail.com>
2024-11-18 13:56:54 +0900

    multi-thread, concurrency tests

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,43 @@
 use tokio::io::{self};
-mod upload_chunk;
-use dotenv::dotenv;
+
+
+async fn sleep_1s_nonblocking(task: &str) {
+  use tokio::time::{sleep, Duration};
+  println!("Entering sleep_1s_nonblocking({task})");
+  sleep(Duration::from_secs(1)).await;
+  println!("Returning from sleep_1s_nonblocking({task})");
+}
+
+#[tokio::main(flavor = "multi_thread", worker_threads = 2)]
+async fn main() {
+  println!("Test 1: Run 2 async tasks squentially");
+  sleep_1s_nonblocking("Task 1").await;
+  sleep_1s_nonblocking("Task 2").await;
+  println!("Test 2: Run 2 async tasks concurrently (same thread)");
+  tokio::join!(sleep_1s_nonblocking("Task 3"), sleep_1s_nonblocking("Task 4"));
+  println!("Test 3: Run 2 async tasks in parallel");
+  tokio::join!(
+    tokio::spawn(sleep_1s_nonblocking("Task 5")),
+    tokio::spawn(sleep_1s_nonblocking("Task 6"))
+  );
+  println!("Test 4: Run 2 async tasks cuncurrently (non-bloking)");
+  sleep_1s_nonblocking("Task 7").await;
+  sleep_1s_nonblocking("Task 8").await;
+
+  println!("Test 5: Run 2 async tasks cuncurrently (same thread, non-bloking)");
+  tokio::join!(
+    sleep_1s_nonblocking("Task 9"),
+    sleep_1s_nonblocking("Task 10")
+  );
+
+  println!("Test 6: Run 2 async tasks in parallel (non-bloking)");
+  tokio::join!(
+    tokio::spawn(sleep_1s_nonblocking("Task 11")),
+    tokio::spawn(sleep_1s_nonblocking("Task 12"))
+  );
 
 
 
-#[tokio::main]
-async fn main() -> io::Result<()> {
-    let file_path = "./module2/import.rs";
-    let upload_url = "https://naver.com";
-    let chunk_size = 1024 * 1024;
-
-    dotenv().ok();
-
-    for (key, value) in std::env::vars() {
-      println!("{}: [{}]", key, value);
-    }
-
-    upload_chunk::upload_file_in_chunks(file_path, upload_url, chunk_size).await?;
-    Ok(())
 
 
 }
